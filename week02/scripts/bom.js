4fbafddc919aaf20be73165e526de3aa880f6088
haly14 <haly14@gmail.com>
2024-06-20 13:56:58 -0600

    edit path favch

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -15,5 +15,30 @@ button.addEventListener('click', () => {
         input.focus();
     });
     input.focus();
-    input.value = '';
+    input.value = ''
 });
+
+
+
+/*
+original BYU-I given answer:
+const input = document.querySelector('#favch');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {}
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+    input.focus();
+    input.value = ''
+});
+*/
