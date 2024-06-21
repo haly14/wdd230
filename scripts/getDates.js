ddacaa80fca3ce200c80f2230ceed81c86700ae2
haly14 <haly14@gmail.com>
2024-06-21 08:56:04 -0600

    first attempt dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,18 @@ document.getElementById('currentYear').textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
