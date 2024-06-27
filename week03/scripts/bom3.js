8d716b0b0a30986c1a18d4fb20f05d2b65c746ef
haly14 <haly14@gmail.com>
2024-06-26 18:37:09 -0600

    week 3 versions of bom html, css, and js

---
+++ week03/scripts/bom3.js
@@ -0,0 +1,48 @@
+const input = document.querySelector('#favch');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {}
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+    input.focus();
+    input.value = ''
+});
+
+
+
+/*
+original BYU-I given answer:
+const input = document.querySelector('#favch');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {}
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+    input.focus();
+    input.value = ''
+});
+
+Other method:
+https://mdn.github.io/learning-area/javascript/apis/document-manipulation/shopping-list-finished.html
+
+*/
