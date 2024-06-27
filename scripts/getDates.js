c1f3969b00ab73b6d099e70bcfde0561167855c7
haly14 <haly14@gmail.com>
2024-06-27 15:53:50 -0600

    fixed class name so no errors when querying the DOM (page visits" to "page visits")

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -58,7 +58,7 @@ let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+	visitsDisplay.textContent = `This is your first visit. Welcome!`;
 }
 
 numVisits++;
