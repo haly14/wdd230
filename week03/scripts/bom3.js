1dfe9bccc978363e971d78d6b578cd322a1565eb
haly14 <haly14@gmail.com>
2024-06-26 18:57:34 -0600

    edits to bom3.js for week 3 assignment on local storage

--- week03/scripts/bom3.js
+++ week03/scripts/bom3.js
@@ -3,46 +3,56 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-    if (input.value != '') {}
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-    li.textContent = input.value;
-    deleteButton.textContent = '❌';
-    li.append(deleteButton);
-    list.append(li);
-    deleteButton.addEventListener('click', function () {
-        list.removeChild(li);
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
         input.focus();
-    });
-    input.focus();
-    input.value = ''
+    }
 });
 
 
 
 /*
-original BYU-I given answer:
-const input = document.querySelector('#favch');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
+Week 3 Activity: Web Storage API
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('chapters')) || null;
+}
+*/
 
-button.addEventListener('click', () => {
-    if (input.value != '') {}
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-    li.textContent = input.value;
-    deleteButton.textContent = '❌';
-    li.append(deleteButton);
-    list.append(li);
-    deleteButton.addEventListener('click', function () {
-        list.removeChild(li);
-        input.focus();
-    });
-    input.focus();
-    input.value = ''
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach (chapter => {
+    displayList(chapter);
 });
 
-Other method:
-https://mdn.github.io/learning-area/javascript/apis/document-manipulation/shopping-list-finished.html
+function displayList(item) {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = item;
+        deleteButton.textContent = '❌';
+        deleteButton.classList.add('delete');
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            deleteChapter(li.textContent);
+            input.focus();
+        });
+    }
+}
 
-*/
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter (chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+}
