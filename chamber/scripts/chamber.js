72eaee2d93ea2d51305b82419bda33a09f362853
haly14 <haly14@gmail.com>
2024-07-01 09:14:33 -0600

    js for hambutton

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -4,3 +4,13 @@ document.getElementById('currentYear').textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+
+// js for hambutton
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+	navElement.classList.toggle('open');
+	//hamburgerElement.classList.toggle('open');
+});
